65ae70f4c82ecb66fc01c8b505342da4254948e3
Igor Santana <igr.amaral@gmail.com>
2021-07-01 17:15:27 -0300

    Refatoração do Client

--- src/Client.php
+++ src/Client.php
@@ -5,23 +5,10 @@ namespace App\Services\Iugu;
 use App\Services\Iugu\Iugu;
 use App\Services\Iugu\RequestHandler;
 use App\Services\Iugu\ResponseHandler;
-use App\Services\Iugu\Endpoints\BankAccounts;
-use App\Services\Iugu\Endpoints\BulkAnticipations;
-use App\Services\Iugu\Endpoints\Transactions;
 use App\Services\Iugu\Endpoints\Customers;
-use App\Services\Iugu\Endpoints\Cards;
-use App\Services\Iugu\Endpoints\Recipients;
-use App\Services\Iugu\Endpoints\PaymentLinks;
-use App\Services\Iugu\Endpoints\Plans;
-use App\Services\Iugu\Endpoints\Transfers;
-use App\Services\Iugu\Endpoints\Subscriptions;
-use App\Services\Iugu\Endpoints\Refunds;
-use App\Services\Iugu\Endpoints\Postbacks;
-use App\Services\Iugu\Endpoints\Balances;
-use App\Services\Iugu\Endpoints\Payables;
-use App\Services\Iugu\Endpoints\BalanceOperations;
-use App\Services\Iugu\Endpoints\Chargebacks;
-use App\Services\Iugu\Endpoints\Search;
+use App\Services\Iugu\Endpoints\Charges;
+use App\Services\Iugu\Endpoints\Invoices;
+use App\Services\Iugu\Endpoints\PaymentToken;
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Exception\ClientException as ClientException;
 use App\Services\Iugu\Exceptions\InvalidJsonException;
@@ -49,89 +36,24 @@ class Client
     private $apiKey;
 
     /**
-     * @var \Iugu\Endpoints\Transactions
-     */
-    private $transactions;
-
-    /**
      * @var \Iugu\Endpoints\Customers
      */
     private $customers;
 
     /**
-     * @var \Iugu\Endpoints\Cards
+     * @var \Iugu\Endpoints\Invoices
      */
-    private $cards;
+    private $invoices;
 
     /**
-     * @var \Iugu\Endpoints\Recipients
+     * @var \Iugu\Endpoints\PaymentToken
      */
-    private $recipients;
+    private $paymentToken;
 
     /**
-     * @var \Iugu\Endpoints\BankAccounts
+     * @var \Iugu\Endpoints\Charges
      */
-    private $bankAccounts;
-
-    /**
-     * @var \Iugu\Endpoints\Plans
-     */
-    private $plans;
-
-    /**
-     * @var \Iugu\Endpoints\BulkAnticipations
-     */
-    private $bulkAnticipations;
-
-    /**
-     * @var \Iugu\Endpoints\PaymentLinks
-     */
-    private $paymentLinks;
-
-    /**
-     * @var \Iugu\Endpoints\Transfers
-     */
-    private $transfers;
-
-    /**
-     * @var \Iugu\Endpoints\Subscriptions
-     */
-    private $subscriptions;
-
-    /**
-     * @var \Iugu\Endpoints\Refunds
-     */
-    private $refunds;
-
-    /**
-     * @var \Iugu\Endpoints\Postbacks
-     */
-    private $postbacks;
-
-    /**
-     * @var \Iugu\Endpoints\Balances
-     */
-    private $balances;
-
-    /**
-     * @var \Iugu\Endpoints\Payables
-     */
-    private $payables;
-
-    /**
-     * @var \Iugu\Endpoints\BalanceOperations
-     */
-    private $balanceOperations;
-
-    /**
-     * @var \Iugu\Endpoints\Chargebacks
-     */
-    private $chargebacks;
-
-    /**
-     * @var \Iugu\Endpoints\Search
-     */
-    private $search;
+    private $charges;
 
     /**
      * @param string $apiKey
@@ -156,23 +78,10 @@ class Client
 
         $this->http = new HttpClient($options);
 
-        $this->transactions = new Transactions($this);
         $this->customers = new Customers($this);
-        $this->cards = new Cards($this);
-        $this->recipients = new Recipients($this);
-        $this->bankAccounts = new BankAccounts($this);
-        $this->plans = new Plans($this);
-        $this->bulkAnticipations = new BulkAnticipations($this);
-        $this->paymentLinks = new PaymentLinks($this);
-        $this->transfers = new Transfers($this);
-        $this->subscriptions = new Subscriptions($this);
-        $this->refunds = new Refunds($this);
-        $this->postbacks = new Postbacks($this);
-        $this->balances = new Balances($this);
-        $this->payables = new Payables($this);
-        $this->balanceOperations = new BalanceOperations($this);
-        $this->chargebacks = new Chargebacks($this);
-        $this->search = new Search($this);
+        $this->invoices = new Invoices($this);
+        $this->paymentToken = new PaymentToken($this);
+        $this->charges = new Charges($this);
     }
 
     /**
@@ -244,14 +153,6 @@ class Client
     }
 
     /**
-     * @return \Iugu\Endpoints\Transactions
-     */
-    public function transactions()
-    {
-        return $this->transactions;
-    }
-
-    /**
      * @return \Iugu\Endpoints\Customers
      */
     public function customers()
@@ -260,122 +161,26 @@ class Client
     }
 
     /**
-     * @return \Iugu\Endpoints\Cards
+     * @return \Iugu\Endpoints\Invoices
      */
-    public function cards()
+    public function invoices()
     {
-        return $this->cards;
+        return $this->invoices;
     }
 
     /**
-     * @return \Iugu\Endpoints\Recipients
+     * @return \Iugu\Endpoints\PaymentToken
      */
-    public function recipients()
+    public function paymentToken()
     {
-        return $this->recipients;
+        return $this->paymentToken;
     }
 
     /**
-     * @return \Iugu\Endpoints\BankAccounts
+     * @return \Iugu\Endpoints\Charges
      */
-    public function bankAccounts()
+    public function charges()
     {
-        return $this->bankAccounts;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Plans
-     */
-    public function plans()
-    {
-        return $this->plans;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\BulkAnticipations
-     */
-    public function bulkAnticipations()
-    {
-        return $this->bulkAnticipations;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\PaymentLinks
-     */
-    public function paymentLinks()
-    {
-        return $this->paymentLinks;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Transfers
-     */
-    public function transfers()
-    {
-        return $this->transfers;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Subscriptions
-     */
-    public function subscriptions()
-    {
-        return $this->subscriptions;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Refunds
-     */
-    public function refunds()
-    {
-        return $this->refunds;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Postbacks
-     */
-    public function postbacks()
-    {
-        return $this->postbacks;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Balances
-     */
-    public function balances()
-    {
-        return $this->balances;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Payables
-     */
-    public function payables()
-    {
-        return $this->payables;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\BalanceOperations
-     */
-    public function balanceOperations()
-    {
-        return $this->balanceOperations;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Chargebacks
-     */
-    public function chargebacks()
-    {
-        return $this->chargebacks;
-    }
-
-    /**
-     * @return \Iugu\Endpoints\Search
-     */
-    public function search()
-    {
-        return $this->search;
+        return $this->charges;
     }
 }
