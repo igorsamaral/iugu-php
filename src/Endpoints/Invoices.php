1d2ca98e007470bf3576f053326118c6d2db87d8
Igor Santana <igr.amaral@gmail.com>
2021-07-01 17:15:53 -0300

    Criação da classe Invoices

---
+++ src/Endpoints/Invoices.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services\Iugu\Endpoints;
+
+use App\Services\Iugu\Routes;
+use App\Services\Iugu\Endpoints\Endpoint;
+
+class Invoices extends Endpoint
+{
+    /**
+     * @param array $payload
+     *
+     * @return \ArrayObject
+     */
+    public function create(array $payload)
+    {
+        return $this->client->request(
+            self::POST,
+            Routes::invoices()->base(),
+            ['json' => $payload]
+        );
+    }
+
+    /**
+     * @param array|null $payload
+     *
+     * @return \ArrayObject
+     */
+    public function getList(array $payload = null)
+    {
+        return $this->client->request(
+            self::GET,
+            Routes::invoices()->base(),
+            ['query' => $payload]
+        );
+    }
+
+    /**
+     * @param array $payload
+     *
+     * @return \ArrayObject
+     */
+    public function get(array $payload)
+    {
+        return $this->client->request(
+            self::GET,
+            Routes::invoices()->details($payload['id'])
+        );
+    }
+
+    /**
+     * @param array $payload
+     *
+     * @return \ArrayObject
+     */
+    public function capture(array $payload)
+    {
+        return $this->client->request(
+            self::POST,
+            Routes::invoices()->capture($payload['id']),
+            ['json' => $payload]
+        );
+    }
+}
